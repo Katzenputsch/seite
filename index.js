53ae86bb53be01ee30e4965d78a6185110453fda
Katzenputsch <32266052+Katzenputsch@users.noreply.github.com>
2018-08-08 12:06:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-var server = app.listen(3000);
+var port = process.env.PORT || 3600;
+var server = app.listen(port);
 
 app.use(express.static("public"));
 console.log("Server bereit...");
